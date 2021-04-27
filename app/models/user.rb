9fe7e2c4acdc147379e9aebc6bf9ed9de4b35156
systementropy <vatsa.saket@gmail.com>
2021-04-28 00:51:51 +0530

    user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
                         uniqueness: {case_sensetive: false}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, 
-                    length: {maximum:25}, 
+                    length: {maximum:40}, 
                     uniqueness: {case_sensetive: false},
                     format: {with: VALID_EMAIL_REGEX}
   has_secure_password
